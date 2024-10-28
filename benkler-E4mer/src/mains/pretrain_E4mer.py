3ca96ddcc60392f238ebe7347402a8b963344aa0
Noam Benkler <nbenkler@sift.net>
2024-10-28 16:18:58 -0400

    fixed main functions to reflect model script changes

--- benkler-E4mer/src/mains/pretrain_E4mer.py
+++ benkler-E4mer/src/mains/pretrain_E4mer.py
@@ -11,7 +11,7 @@ def pretrain_E4mer_base():
         # id_columns=['source_id'],
         finetune=False,
         run_name=f"unlabelled_pretrain_{datetime.today().strftime('%Y-%m-%d %H:%M:%S')}",
-        train_epochs=100
+        train_epochs=20
     )
 
 def finetune_nurse_SSL(nurse):
@@ -20,11 +20,11 @@ def finetune_nurse_SSL(nurse):
         input_columns=['acc_l2_mean', 'hrv_cvsd', 'eda_tonic_mean', 'eda_phasic_mean'],
         # id_columns=['source_id'],
         finetune=True,
-        pretrained_model_dir=os.path.join(ROOT_DIR, "benkler-E4mer/models/unlabelled_pretrain"),
-        checkpoint_dir=os.path.join(ROOT_DIR, f"benkler-E4mer/checkpoint/Nurse{nurse}_SSLFinetune"),
-        save_dir=os.path.join(ROOT_DIR, f"benkler-E4mer/models/Nurse{nurse}_SSLFinetune"), 
+        pretrained_model_dir=os.path.join(ROOT_DIR, "models/unlabelled_pretrain"),
+        checkpoint_dir=os.path.join(ROOT_DIR, f"checkpoint/Nurse{nurse}_SSLFinetune"),
+        save_dir=os.path.join(ROOT_DIR, f"models/Nurse{nurse}_SSLFinetune"), 
         run_name=f"Nurse{nurse}_SSLFinetune_{datetime.today().strftime('%Y-%m-%d %H:%M:%S')}",
-        train_epochs=100
+        train_epochs=20
     )
 
 def main():
