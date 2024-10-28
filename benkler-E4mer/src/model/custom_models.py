d4598391502eabbccacdc1b6f93f682a60d09b9f
Noam Benkler <nbenkler@sift.net>
2024-10-28 16:18:29 -0400

    fixed modeling code for metric computation and added custom model forward function

---
+++ benkler-E4mer/src/model/custom_models.py
@@ -0,0 +1,134 @@
+import torch
+from transformers import PatchTSMixerForTimeSeriesClassification
+from dataclasses import dataclass
+from transformers.utils import ModelOutput
+from typing import Optional, Tuple
+
+@dataclass
+class PatchTSMixerModelOutput(ModelOutput):
+    """
+    Base class for model's outputs, with potential hidden states.
+
+    Args:
+        last_hidden_state (`torch.FloatTensor`  of shape `(batch_size, num_channels, num_patches, d_model)`):
+            Hidden-state at the output of the last layer of the model.
+        hidden_states (`tuple(torch.FloatTensor)`, *optional*):
+            Hidden-states of the model at the output of each layer.
+        patch_input (`torch.FloatTensor` of shape `(batch_size, num_channels, num_patches, patch_length)`):
+            Patched input data to the model.
+        mask: (`torch.FloatTensor` of shape `(batch_size, num_channels, num_patches)`,*optional*):
+            Bool Tensor indicating True in masked patches and False otherwise.
+        loc: (`torch.FloatTensor` of shape `(batch_size, 1, num_channels)`,*optional*):
+            Gives the mean of the context window per channel. Used for revin denorm outside the model, if revin
+            enabled.
+        scale: (`torch.FloatTensor` of shape `(batch_size, 1, num_channels)`,*optional*):
+            Gives the std dev of the context window per channel. Used for revin denorm outside the model, if revin
+            enabled.
+    """
+
+    last_hidden_state: torch.FloatTensor = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    patch_input: torch.FloatTensor = None
+    mask: Optional[torch.FloatTensor] = None
+    loc: Optional[torch.FloatTensor] = None
+    scale: Optional[torch.FloatTensor] = None
+
+
+@dataclass
+class CustomPatchTSMixerForTimeSeriesClassificationOutput(ModelOutput):
+    """
+    Output type of [`PatchTSMixerForTimeSeriesClassificationOutput`].
+
+    Args:
+        prediction_outputs (`torch.FloatTensor` of shape `(batch_size, num_labels)`):
+            Prediction output from the classfication head.
+        last_hidden_state (`torch.FloatTensor` of shape `(batch_size, num_input_channels, num_patches, d_model)`):
+            Backbone embeddings before passing through the head.
+        hidden_states (`tuple(torch.FloatTensor)`, *optional*):
+            Hidden-states of the model at the output of each layer plus the optional initial embedding outputs.
+        loss (*optional*, returned when `y` is provided, `torch.FloatTensor` of shape `()`):
+            Total loss.
+    """
+
+    loss: Optional[torch.FloatTensor] = None
+    prediction_outputs: torch.FloatTensor = None
+    labels: Optional[torch.FloatTensor] = None
+    last_hidden_state: torch.FloatTensor = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+
+class CustomPatchTSMixerForTimeSeriesClassification(PatchTSMixerForTimeSeriesClassification):
+    def forward(
+        self,
+        past_values: torch.Tensor,
+        target_values: torch.Tensor = None,
+        output_hidden_states: Optional[bool] = False,
+        return_loss: bool = True,
+        return_dict: Optional[bool] = None,
+    ) -> CustomPatchTSMixerForTimeSeriesClassificationOutput:
+        r"""
+        target_values (`torch.FloatTensor` of shape `(batch_size, target_len, num_input_channels)` for forecasting,
+            `(batch_size, num_targets)` for regression, or `(batch_size,)` for classification, *optional*): Target
+            values of the time series, that serve as labels for the model. The `target_values` is what the
+            Transformer needs during training to learn to output, given the `past_values`. Note that, this is NOT
+            required for a pretraining task.
+
+            For a forecasting task, the shape is be `(batch_size, target_len, num_input_channels)`. Even if we want
+            to forecast only specific channels by setting the indices in `prediction_channel_indices` parameter,
+            pass the target data with all channels, as channel Filtering for both prediction and target will be
+            manually applied before the loss computation.
+
+            For a classification task, it has a shape of `(batch_size,)`.
+
+            For a regression task, it has a shape of `(batch_size, num_targets)`.
+        return_loss (`bool`, *optional*):
+            Whether to return the loss in the `forward` call.
+
+        Returns:
+
+        """
+
+        loss = torch.nn.CrossEntropyLoss()
+
+        return_dict = return_dict if return_dict is not None else self.use_return_dict
+
+        model_output = self.model(
+            past_values,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )  # x: [batch_size x nvars x num_patch x d_model]
+        if isinstance(model_output, tuple):
+            model_output = PatchTSMixerModelOutput(*model_output)
+
+        if self.inject_scale is not None:
+            model_output.last_hidden_state = self.inject_scale(
+                model_output.last_hidden_state,
+                loc=model_output.loc,
+                scale=model_output.scale,
+            )  # x: [batch_size x nvars x num_patch x d_model]
+
+        y_hat = self.head(model_output.last_hidden_state)  # tensor [batch_size x n_labels]
+
+        if target_values is not None and return_loss is True:
+            loss_val = loss(y_hat, target_values)
+        else:
+            loss_val = None
+
+        if not return_dict:
+            return tuple(
+                v
+                for v in [
+                    loss_val,
+                    target_values,
+                    y_hat,
+                    model_output.last_hidden_state,
+                    model_output.hidden_states,
+                ]
+            )
+
+        return CustomPatchTSMixerForTimeSeriesClassificationOutput(
+            loss=loss_val,
+            prediction_outputs=y_hat,  # tensor [batch_size x n_labels]
+            labels = target_values,
+            last_hidden_state=model_output.last_hidden_state,  # x: [batch_size x nvars x num_patch x d_model]
+            hidden_states=model_output.hidden_states,
+        )
--- benkler-E4mer/src/model/model.py
+++ benkler-E4mer/src/model/model.py
@@ -1,17 +1,20 @@
 import os
 import torch
+import numpy as np
 from transformers import (
     PatchTSMixerForPretraining,
     PatchTSMixerForTimeSeriesClassification,
     Trainer,
 )
 import requests
-from src.model.data import fetch_data#fetch_next_batch, preprocess_batch
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from src.model.data import fetch_data, clean_data#fetch_next_batch, preprocess_batch
 from src.data import preprocess
-from src.model.config import configure_model, pretrain_training_args, classify_training_args
-from src.model.utils import setup_early_stopping, evaluate_and_save_model, compute_metrics
-from src.STATIC import ROOT_DIR
+from src.model.config import configure_masked_transformer, configure_classifier, pretrain_training_args, classify_training_args
+from src.model.utils import setup_early_stopping, evaluate_and_save_model
+from src.model.custom_models import CustomPatchTSMixerForTimeSeriesClassification
 
+from src.STATIC import ROOT_DIR
 
 def train_model(model, train_dataset, val_dataset, training_args, early_stopping_callback, compute_metrics = None):
     """Initializes and trains the model using the Trainer API."""
@@ -34,6 +37,84 @@ def train_model(model, train_dataset, val_dataset, training_args, early_stopping
     trainer.train()
     return trainer
 
+def masked_trainer(model, train_dataset, val_dataset, training_args, early_stopping_callback):
+    """Initializes and trains the model using the Trainer API."""
+    
+#     def compute_metrics(eval_pred):
+#         # Unpack predictions and labels (if available)
+#         logits, labels = eval_pred
+#         # Compute the loss or any other metric here
+#         loss = trainer.compute_loss(model, torch.tensor(logits), torch.tensor(labels))  # Example
+#         return {"eval_loss": loss}
+    
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        # compute_metrics = compute_metrics,
+        callbacks=[early_stopping_callback],
+    )
+    eval_results = trainer.evaluate()
+    print(f"Evaluation Results: {eval_results}")
+
+    trainer.train()
+    return trainer
+
+def classifier_trainer(model, train_dataset, val_dataset, training_args, early_stopping_callback):
+    """Initializes and trains the model using the Trainer API."""
+
+    def compute_metrics(eval_pred):
+        print("Computing Metrics")
+        output, _ = eval_pred
+
+        # Check if logits need reshaping
+        if isinstance(output, tuple):
+            logits = output[0]  # Unnest if needed
+            labels = output[1]
+        else:
+            logits, labels = output
+        print(f"logits: {logits}, labels: {labels}")
+        
+        if len(logits.shape) > 2:
+            logits = logits.squeeze()  # Flatten if necessary
+
+        predictions = np.argmax(logits, axis=-1)
+        labels = labels
+        print(f"predictions: {predictions}, labels: {labels}")
+        
+        # Calculate accuracy, precision, recall, F1
+        acc = accuracy_score(labels, predictions)
+        print(f"acc: {labels}")
+        precision, recall, f1, _ = precision_recall_fscore_support(labels, predictions, average='weighted')
+        print(f"f1: {f1}")
+        
+        # Compute loss directly using model’s criterion (cross-entropy loss here as an example)
+        loss_fn = torch.nn.CrossEntropyLoss()
+        loss = loss_fn(torch.tensor(logits), torch.tensor(labels)).item()
+        print(f"loss: {loss}")
+        
+        metrics = {
+                    "eval_accuracy": acc,
+                    "eval_precision": precision,
+                    "eval_recall": recall,
+                    "eval_f1": f1,
+                    "eval_loss": loss,
+                }
+        print(f"Metrics returned by compute_metrics: {metrics}")
+        return metrics
+    
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        compute_metrics = compute_metrics,
+        callbacks=[early_stopping_callback]
+    )
+    trainer.train()
+    return trainer
+
 
 # Main function to initiate pretraining
 def pretrain(dataset_code,
@@ -42,13 +123,13 @@ def pretrain(dataset_code,
              finetune = False,
             pretrained_model_dir = None,
             checkpoint_dir=os.path.join(ROOT_DIR, "checkpoint/unlabelled_pretrain"),
-             save_dir=os.path.join(ROOT_DIR, "models/unlabelled_pretrain"), 
+             save_dir=os.path.join(ROOT_DIR, "models/unlabelled_pretrain"),
              run_name = 'unlabelled_pretrain',
-             train_epochs=100, 
+              train_epochs=100, 
              context_length=512, prediction_length=96, patch_length=8,
              num_workers=16, batch_size=16):
     
-    config = configure_model(input_columns, context_length, prediction_length, patch_length)
+    config = configure_masked_transformer(input_columns, context_length, prediction_length, patch_length)
     training_args = pretrain_training_args(checkpoint_dir, train_epochs, batch_size, num_workers, input_columns, run_name = run_name)
     early_stopping_callback = setup_early_stopping()
     
@@ -67,7 +148,8 @@ def pretrain(dataset_code,
                                                                                 id_columns = id_columns)
 
     # Train model in batches to handle memory and storage constraints
-    trainer = train_model(model, train_dataset, val_dataset, training_args, early_stopping_callback)
+    # trainer = train_model(model, train_dataset, val_dataset, training_args, early_stopping_callback)
+    trainer = masked_trainer(model, train_dataset, val_dataset, training_args, early_stopping_callback)
 
     # Evaluate and save model
     evaluate_and_save_model(trainer, test_dataset, save_dir)
@@ -79,47 +161,41 @@ def train_classifier(dataset_code,
                         pretrained_model_dir = os.path.join(ROOT_DIR, "models/unlabelled_pretrain"),
                          checkpoint_dir=os.path.join(ROOT_DIR, "checkpoint/stress_event_finetune"),
                          save_dir=os.path.join(ROOT_DIR, "models/stress_event_finetune"), 
-                         run_name = 'classifier_finetune',
+                        run_name = 'classifier_finetune',
                          train_epochs=100, 
-                         context_length=512, prediction_length=96,
+                         context_length=512, #prediction_length=96,
                          patch_length=8, num_workers=16, batch_size=16):
+    # Data Preprocessing
+    data_loc = 'local' if os.path.exists(os.path.join(ROOT_DIR, f'./e4data/train_test_split/{dataset_code}')) else 'remote'
+    train_data, val_data, test_data = fetch_data(dataset_code, location = data_loc)
+    train_data = clean_data(train_data, input_columns=input_columns, label_column=target_columns)
+    val_data = clean_data(val_data, input_columns=input_columns, label_column=target_columns)
+    test_data = clean_data(test_data, input_columns=input_columns, label_column=target_columns)
+    _, train_dataset, val_dataset, test_dataset = preprocess.preprocess_finetuning_datasets(
+                                                                                train_data,
+                                                                                val_data,
+                                                                                test_data,
+                                                                                target_columns = target_columns,
+                                                                                id_columns = id_columns
+                                                                                )
     
-    config = configure_model(input_columns, context_length, prediction_length, patch_length)
+    # Model Training Config
+    config = configure_classifier(input_columns, context_length, #prediction_length,
+                                   patch_length)
     training_args = classify_training_args(checkpoint_dir, train_epochs, batch_size, num_workers,
-                                            input_columns,target_columns,
+                                            input_columns,
+                                            target_columns,
                                             run_name = run_name
                                           )
     early_stopping_callback = setup_early_stopping()
 
     if finetune and os.path.exists(pretrained_model_dir):
-        model = PatchTSMixerForTimeSeriesClassification(config).from_pretrained(pretrained_model_dir)
+        model = CustomPatchTSMixerForTimeSeriesClassification(config).from_pretrained(pretrained_model_dir)
     else:
-        model = PatchTSMixerForTimeSeriesClassification(config)
-
-
-    data_loc = 'local' if os.path.exists(os.path.join(ROOT_DIR, f'./e4data/train_test_split/{dataset_code}')) else 'remote'
-    train_data, val_data, test_data = fetch_data(dataset_code, location = data_loc)
-    _, train_dataset, val_dataset, test_dataset = preprocess.preprocess_finetuning_datasets(
-                                                                                train_data,
-                                                                                val_data,
-                                                                                test_data,                                              
-                                                                                target_columns = target_columns,
-                                                                                id_columns = id_columns)
+        model = CustomPatchTSMixerForTimeSeriesClassification(config)
 
     # Train model in batches to handle memory and storage constraints
-    trainer = train_model(model, train_dataset, val_dataset, training_args, early_stopping_callback,
-                          compute_metrics = compute_metrics)
+    trainer = classifier_trainer(model, train_dataset, val_dataset, training_args, early_stopping_callback)
 
     # Evaluate and save model
     evaluate_and_save_model(trainer, test_dataset, save_dir)
-
-
-
-
-# def main():
-#     # Initiate pretraining process
-#     pretrain(input_columns=['acc_l2_mean', 'hrv_cvsd', 'eda_tonic_mean', 'eda_phasic_mean'], train_epochs=1)
-#     # train_classifier(WESAD_train_dataset, WESAD_val_dataset, WESAD_test_dataset,
-#     #                  input_columns = ['acc_l2_mean','hrv_cvsd','eda_tonic_mean','eda_phasic_mean'],
-#     #                 target_columns = ['binary_stress'],
-#     #                  train_epochs = 1)
