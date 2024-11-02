46a4b317f327c779b1ca0f54f9f3aea374c753b4
Noam Benkler <nbenkler@sift.net>
2024-11-02 12:44:27 -0400

    added streaming test

---
+++ benkler-E4mer/src/data/streaming_test.py
@@ -0,0 +1,14 @@
+import requests
+from src.STATIC import TARGET_IP, PORT, API_KEY
+
+def test():
+    response = requests.get(
+                f"http://{TARGET_IP}:{PORT}/test_connection",
+                headers={"x-api-key": API_KEY}
+            )
+    response.raise_for_status()
+    print(response.json())
+
+
+if __name__ == '__main__':
+    test()
