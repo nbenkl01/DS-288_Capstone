aeed1c399cb19e114ebaec34a903d3181259097c
Noam Benkler <nbenkler@sift.net>
2024-11-02 14:08:24 -0400

    adding data streaming test

--- benkler-E4mer/src/data/streaming_test.py
+++ benkler-E4mer/src/data/streaming_test.py
@@ -1,4 +1,5 @@
 import requests
+from src.model.data import fetch_data
 from src.STATIC import TARGET_IP, PORT, API_KEY
 
 def test():
@@ -10,9 +11,22 @@ def test():
     response.raise_for_status()
     print(response.json())
 
-    print('Testing Data Streaming')
+    print('Testing Toy Data Streaming')
     response = requests.get(
                 f"http://{TARGET_IP}:{PORT}/test_data_streaming",
+                headers={"x-api-key": API_KEY}
+            )
+    response.raise_for_status()
+    print(response.json())
+
+    print('Testing Real Data Streaming')
+    response = requests.get(
+                f"http://{TARGET_IP}:{PORT}/get_datasets",
+                params={
+                    "dataset_code": 'WESAD',
+                    "batch_size": 100,
+                    "offset": 0
+                },
                 headers={"x-api-key": API_KEY}
             )
     response.raise_for_status()
