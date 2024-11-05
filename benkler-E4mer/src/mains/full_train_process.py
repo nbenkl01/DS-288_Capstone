3bba5e581abae4941157b7dec53412ace228c1c5
Noam Benkler <nbenkler@sift.net>
2024-11-04 19:38:04 -0500

    added train_Nurses_benchmark to full train main

--- benkler-E4mer/src/mains/full_train_process.py
+++ benkler-E4mer/src/mains/full_train_process.py
@@ -2,7 +2,7 @@
 from src.model import model as model
 # from src.STATIC import ROOT_DIR
 from src.mains.pretrain_E4mer import pretrain_E4mer_base, finetune_nurse_SSL
-from src.mains.train_stress_classifier import train_WESAD_benchmark, finetune_stress_E4mer
+from src.mains.train_stress_classifier import train_WESAD_benchmark, train_Nurses_benchmark, finetune_stress_E4mer
 
 NURSES = ['94', 'DF', 'F5', 'E4', '7E', 
           '6B', '5C', '6D', 'BG', 'EG', 'CE',
@@ -13,6 +13,7 @@ def main():
     train_WESAD_benchmark()
     finetune_stress_E4mer(pretrained_model='unlabelled_pretrain')
     # nurses = os.listdir(os.path.join(ROOT_DIR,'e4data/train_test_split/Nurses'))
+    train_Nurses_benchmark()
     for nurse in NURSES: #listdir will only work on local machine
         finetune_nurse_SSL(nurse)
         finetune_stress_E4mer(pretrained_model = f"Nurse{nurse}_SSLFinetune")
