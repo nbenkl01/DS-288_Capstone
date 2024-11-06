60e9f0f2740dbd1001ab4703553839ae54279a0d
Noam Benkler <nbenkler@sift.net>
2024-11-05 21:07:47 -0500

    added some default args to config for later use maybe

--- benkler-E4mer/src/utils/config.py
+++ benkler-E4mer/src/utils/config.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from more_itertools import collapse
 from datetime import datetime
@@ -86,3 +87,125 @@ class Config:
                 setattr(self, key, value)
             else:
                 raise AttributeError(f"{key} is not a valid attribute of Config.")
+    
+    @staticmethod
+    def from_args():
+        parser = argparse.ArgumentParser(description="Configuration arguments for the model training")
+
+        # Define all arguments for each parameter in the Config class
+        parser.add_argument('--dataset_code', type=str, required=True, help="Dataset code")
+        parser.add_argument('--task', type=str, required=True, help="Task type")
+        parser.add_argument('--input_columns', type=str, nargs='+', required=True, help="List of input columns")
+        parser.add_argument('--target_columns', type=str, nargs='*', default=None, help="List of target columns")
+        parser.add_argument('--id_columns', type=str, nargs='*', default=None, help="List of ID columns")
+        parser.add_argument('--timestamp_column', type=str, default='datetime', help="Timestamp column name")
+        parser.add_argument('--finetune', action='store_true', help="Whether to finetune the model")
+        parser.add_argument('--pretrained_model_dir', type=str, default=None, help="Path to pretrained model")
+        parser.add_argument('--checkpoint_dir', type=str, default=None, help="Path to checkpoint directory")
+        parser.add_argument('--save_dir', type=str, default=None, help="Directory to save model")
+        parser.add_argument('--run_name', type=str, default=None, help="Run name")
+        parser.add_argument('--context_length', type=int, default=512, help="Context length")
+        parser.add_argument('--prediction_length', type=int, default=96, help="Prediction length")
+        parser.add_argument('--patch_length', type=int, default=8, help="Patch length")
+        parser.add_argument('--stride', type=int, default=16, help="Stride length")
+        parser.add_argument('--data_batch_size', type=int, default=5000, help="Batch size for data")
+        parser.add_argument('--batch_train', type=bool, default=None, help="Whether to use batch for training")
+        parser.add_argument('--batch_val', type=bool, default=None, help="Whether to use batch for validation")
+        parser.add_argument('--train_epochs', type=int, default=100, help="Number of epochs for training")
+        parser.add_argument('--num_workers', type=int, default=16, help="Number of workers for data loading")
+        parser.add_argument('--batch_size', type=int, default=16, help="Batch size")
+        parser.add_argument('--eval_metric', type=str, default='eval_f1', help="Evaluation metric")
+        parser.add_argument('--greater_is_better', type=bool, default=True, help="Whether higher eval metric is better")
+
+        args = parser.parse_args()
+
+        # Return a Config object initialized with the parsed arguments
+        return Config(
+            dataset_code=args.dataset_code,
+            task=args.task,
+            input_columns=args.input_columns,
+            target_columns=args.target_columns,
+            id_columns=args.id_columns,
+            timestamp_column=args.timestamp_column,
+            finetune=args.finetune,
+            pretrained_model_dir=args.pretrained_model_dir,
+            checkpoint_dir=args.checkpoint_dir,
+            save_dir=args.save_dir,
+            run_name=args.run_name,
+            context_length=args.context_length,
+            prediction_length=args.prediction_length,
+            patch_length=args.patch_length,
+            stride=args.stride,
+            data_batch_size=args.data_batch_size,
+            batch_train=args.batch_train,
+            batch_val=args.batch_val,
+            train_epochs=args.train_epochs,
+            num_workers=args.num_workers,
+            batch_size=args.batch_size,
+            eval_metric=args.eval_metric,
+            greater_is_better=args.greater_is_better
+        )
+    
+# Default arguments
+WESAD_DEFAULT_ARGS = {
+    'dataset_code': 'WESAD',
+    'task': 'classification',
+    'input_columns': ['acc_l2_mean','hrv_cvsd','eda_tonic_mean','eda_phasic_mean'],
+    'target_columns': 'binary_stress',
+    'id_columns': ['subject_id','condition'],
+    'finetune': False,
+    'checkpoint_dir': os.path.join(ROOT_DIR, "checkpoint/stress_event_baseline/WESAD"),
+    'save_dir': os.path.join(ROOT_DIR, "models/stress_event_baseline/WESAD"),
+    'run_name': f"WESAD_benchmark_{datetime.today().strftime('%Y-%m-%d %H:%M:%S')}",
+    'batch_train': False,
+    'batch_val': False,
+    'train_epochs': 100,
+    'num_workers': 16,
+    'batch_size': 16,
+    'context_length': 512,
+    'prediction_length': 96,
+    'patch_length': 8,
+    'stride': 16,
+}
+
+Nurses_CLASS_ARGS = WESAD_DEFAULT_ARGS.copy()
+Nurses_CLASS_ARGS.update({
+            'dataset_code': 'Nurses/labelled',
+            'id_columns': ['subject_id', 'session_id'],
+            'checkpoint_dir': os.path.join(ROOT_DIR, "checkpoint/stress_event_baseline/Nurses"),
+            'save_dir': os.path.join(ROOT_DIR, "models/stress_event_baseline/Nurses"),
+            'run_name': f"Nurses_benchmark_{datetime.today().strftime('%Y-%m-%d %H:%M:%S')}",
+        })
+
+WESAD_FINETUNE_ARGS = WESAD_DEFAULT_ARGS.copy()
+WESAD_FINETUNE_ARGS.update({
+            'finetune': True,
+            'pretrained_model_dir': os.path.join(ROOT_DIR, "models", "unlabelled_pretrain"),
+            'checkpoint_dir': os.path.join(ROOT_DIR, "checkpoint/stress_event_finetune/unlabelled_pretrain"),
+            'save_dir': os.path.join(ROOT_DIR, "models/stress_event_finetune/unlabelled_pretrain"),
+            'run_name': f"WESAD_unlabelled_finetune_{datetime.today().strftime('%Y-%m-%d %H:%M:%S')}",
+        })
+
+UNLABELLED_DEFAULT_ARGS = {
+    'dataset_code': 'unlabelled',
+    'task': 'masked_prediction',
+    'input_columns': ['acc_l2_mean', 'hrv_cvsd', 'eda_tonic_mean', 'eda_phasic_mean'],
+    'id_columns': ['source_id'],
+    'finetune': False,
+    'checkpoint_dir': os.path.join(ROOT_DIR, "checkpoint/unlabelled_pretrain"),
+    'save_dir': os.path.join(ROOT_DIR, "models/unlabelled_pretrain"),
+    'run_name': f"unlabelled_pretrain_{datetime.today().strftime('%Y-%m-%d %H:%M:%S')}",
+    'stride': 1,
+}
+
+Nurses_FINETUNE_ARGS = UNLABELLED_DEFAULT_ARGS.copy()
+Nurses_FINETUNE_ARGS.update({
+    'dataset_code': f"Nurses/unlabelled",
+    'id_columns': ['subject_id', 'session_id'],
+    'finetune': True,
+    'pretrained_model_dir': os.path.join(ROOT_DIR, "models/unlabelled_pretrain"),
+    'checkpoint_dir': os.path.join(ROOT_DIR, f"checkpoint/Nurses_SSLFinetune"),
+    'save_dir': os.path.join(ROOT_DIR, f"models/Nurses_SSLFinetune"),
+    'run_name': f"Nurses_SSLFinetune_{datetime.today().strftime('%Y-%m-%d %H:%M:%S')}",
+    'batch_train': False,
+})
