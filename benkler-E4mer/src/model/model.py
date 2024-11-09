cd4c2626099f9f82bc5c34745cd5ec5f5d59a0ef
Noam Benkler <nbenkler@sift.net>
2024-11-09 13:12:24 -0500

    reversed previous change

--- benkler-E4mer/src/model/model.py
+++ benkler-E4mer/src/model/model.py
@@ -5,18 +5,17 @@ from transformers import (
 )
 from src.model.data import get_data, clean_data, preprocess
 from src.model.config import configure_model, configure_training_args
-from src.model.utils import setup_early_stopping, evaluate_and_save_model, compute_metrics, initialize_optimizer_and_scheduler
+from src.model.utils import setup_early_stopping, evaluate_and_save_model, compute_metrics
 from src.model.custom_models import CustomPatchTSMixerForTimeSeriesClassification
 
-def initialize_trainer(model, training_args, train_dataset, val_dataset, early_stopping_callback, config, optimizer, scheduler):
+def initialize_trainer(model, training_args, train_dataset, val_dataset, early_stopping_callback, config):
     """Initializes the Trainer with common configurations."""
     trainer_params = {
             'model':model,
             'args':training_args,
             'train_dataset':train_dataset,
             'eval_dataset':val_dataset,
-            'callbacks':[early_stopping_callback],
-            'optimizers': (optimizer, scheduler)
+            'callbacks':[early_stopping_callback]
         }
     
     if config.task == 'classification':
@@ -33,7 +32,6 @@ def train_model(model, training_args, early_stopping_callback, config):
     batch_index = 0
     trainer = None
     tsp=None
-    optimizer, scheduler = initialize_optimizer_and_scheduler(model, training_args, config)
     
     while config.batch_train:
         if config.batch_val:
@@ -61,7 +59,7 @@ def train_model(model, training_args, early_stopping_callback, config):
         _, val_dataset = preprocess(val_data, config, tsp=tsp, fit = False)
         
         if trainer is None:
-            trainer = initialize_trainer(model, training_args, train_dataset, val_dataset, early_stopping_callback, config, optimizer, scheduler)
+            trainer = initialize_trainer(model, training_args, train_dataset, val_dataset, early_stopping_callback, config)
         else:
             trainer.train_dataset, trainer.eval_dataset = train_dataset, val_dataset
         
@@ -74,7 +72,7 @@ def train_model(model, training_args, early_stopping_callback, config):
         train_data, val_data = map(lambda data: clean_data(data, config), [train_data, val_data])
         tsp, train_dataset = preprocess(train_data, config, tsp=None, fit = True)
         _, val_dataset = preprocess(val_data, config, tsp=tsp, fit = False)
-        trainer = initialize_trainer(model, training_args, train_dataset, val_dataset, early_stopping_callback, config, optimizer, scheduler)
+        trainer = initialize_trainer(model, training_args, train_dataset, val_dataset, early_stopping_callback, config)
         trainer.train()
 
     return trainer, tsp
